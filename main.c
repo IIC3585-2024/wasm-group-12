2e4a01cc35b875fe416757ecc72cfaf15796e786
scavagnaro <sebastian@xpendit.com>
2024-04-14 22:13:37 -0400

    feature: added long integer support

--- main.c
+++ main.c
@@ -2,23 +2,47 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-bool isFactor(int num, int factor);
+bool isFactor(long num, long factor);
 
-bool isPrime(int num);
+bool isPrime(long num);
 
-int* numberDecompose(int num);
+long* numberDecompose(long num);
 
-void numberDecomposeRecursive(int num, int i, int** primes, int* count);
+// int main() {
+//     // long num;
+//     // prlongf("Enter an longeger: ");
+//     // scanf("%d", &num);
 
-void numberDecomposeRecursive(int num, int i, int** primes, int* count) {
+//     // prlongf("Prime numbers between 1 and %d are: ", num);
+//     // for(long i = 2; i <= num; i++) {
+//     //     if (isFactor(num, i) && isPrime(i)) {
+//     //         prlongPrime(num, i);
+//     //     }
+//     // }
+
+//     long* array = numberDecompose(0); 
+
+//     for (long i = 0; array[i] != -1; i++) {
+//         printf("%ld\n", array[i]);
+//     }
+
+//     return 0;
+// }
+
+void numberDecomposeRecursive(long num, long i, long** primes, long* count);
+
+void numberDecomposeRecursive(long num, long i, long** primes, long* count) {
     if (i > num) {
         return;
     }
+    
+    // printf("num: %ld, i: %ld\n", num, i);
+    // printf("isFactor: %d, isPrime: %d\n", isFactor(num, i), isPrime(i));
 
     if (isFactor(num, i) && isPrime(i)) {
         num = num / i;
         (*count)++;
-        *primes = realloc(*primes, (*count + 1) * sizeof(int));
+        *primes = realloc(*primes, (*count + 1) * sizeof(long));
         (*primes)[*count - 1] = i;
         numberDecomposeRecursive(num, i, primes, count);
     } else {
@@ -26,9 +50,14 @@ void numberDecomposeRecursive(int num, int i, int** primes, int* count) {
     }
 }
 
-int* numberDecompose(int num) {
-    int count = 0;
-    int* primes = NULL;
+long* numberDecompose(long num) {
+
+
+
+    long count = 0;
+    long* primes;
+
+    
 
     numberDecomposeRecursive(num, 2, &primes, &count);
 
@@ -36,13 +65,13 @@ int* numberDecompose(int num) {
     return primes;
 }
 
-bool isFactor(int num, int factor) {
+bool isFactor(long num, long factor) {
     return num % factor == 0;
 }
 
-bool isPrime(int num) {
+bool isPrime(long num) {
     if (num <= 1) return false;
-    for(int i = 2; i <= num / 2; i++) {
+    for(long i = 2; i <= num / 2; i++) {
         if(num % i == 0) {
             return false;
         }
